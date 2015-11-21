81f689d97cbc521d4d12b3ac3a758ec6dc0c55b0
ReemAlattas <ralattas@live.com>
2015-11-21 16:00:55 -0500

    create sessions variables

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '902301929865813', 'a103fa8ee8746214e8f79ded12c556d7'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/failure'
+
   get 'welcome/index'
   root 'welcome#index'
 
