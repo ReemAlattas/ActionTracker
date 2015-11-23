c919b503f8c2b19d69c66599ea0e6d5e361f3fd3
nichol alexander <nichol.alexander@gmail.com>
2015-11-23 02:54:10 -0500

    update auth details in config/initializers/devise.rb - following guidelines from devise documentation at https://github.com/plataformatec/devise/wiki/OmniAuth:-Overview

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,8 +13,6 @@ Devise.setup do |config|
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
 
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '902301929865813', 'a103fa8ee8746214e8f79ded12c556d7'
-  end
+  config.omniauth :facebook, '902301929865813', 'a103fa8ee8746214e8f79ded12c556d7'
 
 end
